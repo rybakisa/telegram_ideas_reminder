9d06a998c6874fcb8975cb460921f89fc799847f
Andrey Ezhov <rybakisa@gmail.com>
2021-07-31 02:20:03 +0300

    Remade Telegram chats handling and scheduler delay calculation
    Change Telegram chats identification to t.me links
    Add casting to delay calculation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sched, time
-
 import settings
 from scheduler import Scheduler
 from random_message_sender import RandomMessageSender
@@ -8,8 +6,8 @@ from random_message_sender import RandomMessageSender
 sender = RandomMessageSender(
     api_id=settings.TELEGRAM_API_ID,
     api_hash=settings.TELEGRAM_API_HASH,
-    from_chat_id=settings.TELEGRAM_FROM_CHAT_ID,
-    to_chat_id=settings.TELEGRAM_TO_CHAT_ID,
+    send_from=settings.TELEGRAM_FROM_CHAT_LINK,
+    send_to=settings.TELEGRAM_TO_CHAT_LINK,
 )
 scheduler = Scheduler(sender.run, settings.SCHEDULER_DELAY_SECONDS)
 
--- random_message_sender.py
+++ random_message_sender.py
@@ -3,25 +3,26 @@ from telethon import TelegramClient
 
 
 class RandomMessageSender:
-    def __init__(self, api_id, api_hash, from_chat_id, to_chat_id):
+    def __init__(self, api_id, api_hash, send_from, send_to):
         self.client = TelegramClient(
             'anon',
             api_id=api_id,
             api_hash=api_hash
         )
-        self.from_chat_id = from_chat_id
-        self.to_chat_id = to_chat_id
+        self.send_from = send_from
+        self.send_to = send_to
 
-    async def __send_random_message(self, from_chat_id, to_chat_id):
-        messages = await self.client.get_messages(from_chat_id, limit=None)
+    async def __send_random_message(self):
+        dialogs = await self.client.get_dialogs()
+        send_from_entity = await self.client.get_entity(self.send_from)
+        send_to_entity = await self.client.get_entity(self.send_to)
+
+        messages = await self.client.get_messages(send_from_entity, limit=None)
         random_message = random.choice(messages)
-        await self.client.forward_messages(to_chat_id, random_message)
+        await self.client.forward_messages(entity=send_to_entity, messages=random_message, from_peer=send_from_entity)
 
     def run(self):
         with self.client as client:
             client.loop.run_until_complete(
-                self.__send_random_message(
-                    self.from_chat_id,
-                    self.to_chat_id,
-                )
+                self.__send_random_message()
             )
